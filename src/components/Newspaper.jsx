26fbd20e8889cc0e37ac919267867d4df0e9a995
yokoOkubo <yoko_okubo@yahoo.co.jp>
2023-11-04 15:39:30 +0900

    ほぼ基本機能を実装

--- src/components/Newspaper.jsx
+++ src/components/Newspaper.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Newspaper.scss"
+import { collection, onSnapshot } from 'firebase/firestore';
+import db from '../firebase';
 
 const Newspaper = () => {
+  const [papers, setPaper] = useState([]);
+
+  useEffect(()=> {
+    const dayori = collection(db, "dayori");
+    onSnapshot(dayori,(snapshot)=>{
+      setPaper(snapshot.docs.map((doc)=> {
+        return {...doc.data()};
+      }))
+    });
+
+
+  },[]);
   return (
     <div className="newspaper">
-      <iframe 
-        src="西三会だより2023年1月号.pdf"
-        width="100%"
-        height="100%"
-        className="one"
-      >
-
-      </iframe>
-      <iframe 
-        src="西三会だより2023年2月号.pdf"
-        width="100%"
-        height="100%"
-        className="one"
-      >
-
-      </iframe>
+      {papers.map((paper) => (
+        <div className="titles" key={paper.title}>
+          <a href={paper.url} target="_blank" className="one">
+            {paper.title}
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
